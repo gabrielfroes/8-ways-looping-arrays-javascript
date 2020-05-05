551724ec0a3e1af4178d33e34c74a44185d718a9
Gabriel Froes <gabriel@rwstudio.com.br>
2020-05-05 19:45:44 -0300

    Test with "continue"

--- for.js
+++ for.js
@@ -12,3 +12,8 @@ for (let index = 0; index < bolsaVanessa.length; index++) {
     console.log(`${index+1}. ${bolsaVanessa[index]}`)
     if (bolsaVanessa[index] == 'dinheiro') break
 }
+
+for (let index = 0; index < bolsaVanessa.length; index++) {
+    if (bolsaVanessa[index] == 'dinheiro') continue
+    console.log(`${index+1}. ${bolsaVanessa[index]}`)
+}
